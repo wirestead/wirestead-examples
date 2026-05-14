da4233443d80fdd6de75eac613c41e68a7da86fc
Jinwoo Sung <jwsung91@gmail.com>
2026-05-14 21:43:24 +0900

    ci: add code formatting check

---
+++ .cmake-format.py
@@ -0,0 +1,13 @@
+with section("format"):
+    line_width = 80
+    max_subgroups_hwrap = 2
+    max_pargs_hwrap = 6
+    max_rows_cmdline = 2
+    dangle_parens = True
+    enable_sort = True
+    autosort = True
+
+# Options for file encoding
+with section("encode"):
+    input_encoding = "utf-8"
+    output_encoding = "utf-8"
